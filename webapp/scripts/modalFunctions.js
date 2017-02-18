921c91389409e4ea63792907893b8c7d828e9e1f
arpiquette <arpiquette@yahoo.com>
2017-02-17 19:58:45 -0800

    Changed CSS for welcome modal.

--- webapp/scripts/modalFunctions.js
+++ webapp/scripts/modalFunctions.js
@@ -2,7 +2,7 @@ $("#menu-toggle").click(function(e){e.preventDefault();$("#wrapper").toggleClass
 
 //Modal Functions - authored by Anthony Piquette
 // Get the modal
-var modal2 = document.getElementById('welcomeModal');
+var modalopen = document.getElementById('welcomeModal');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close2")[0];
@@ -20,37 +20,37 @@ var closeSideBar = document.getElementById('slidebar-white');
 // Modal opens on page load
 function welcomeModal() {
     if(sessionStorage.getItem("siteVisits") > 1){
-        modal2.style.display = "none";
+        modalopen.style.display = "none";
     } else {
-        modal2.style.display = "block";
+        modalopen.style.display = "block";
     }
 }
 window.onload =welcomeModal;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal2.style.display = "none";
+    modalopen.style.display = "none";
 }
 
 // When the user clicks on <Hamburger Menu> Modal is closed
 hamburgerClose.onclick = function(){
-    modal2.style.display ="none";
+    modalopen.style.display ="none";
     closeSideBar.style.display = "block";
 }
 // When the user clicks on <Search Bar> Modal is closed
 searchBarClose.onclick = function(){
-    modal2.style.display ="none";
+    modalopen.style.display ="none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal2) {
-        modal2.style.display = "none";
+        modalopen.style.display = "none";
     }
 }
 
 // When user clicks on welcome button in navigation menu, the welcome modal is displayed 
 openWelcomeModal.onclick = function(){
-        modal2.style.display = "block";
+        modalopen.style.display = "block";
         closeSideBar.style.display = "none";
     }
