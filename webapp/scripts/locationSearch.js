95265edc20b7051c5dabe0fedd88177221c5e16d
testmonkey101 <aaron.briggs@gmail.com>
2017-03-06 20:17:16 -0800

    Workaround implemented to remove placeholder for serach, and require a minimum of one character for the search list to appear. Closes #106 Redundant files removed as part of cleanup.

--- webapp/scripts/locationSearch.js
+++ webapp/scripts/locationSearch.js
@@ -1,6 +1,6 @@
 var locationSearch = new autoComplete({
             selector: '#advanced-demo',
-            minChars: 0,
+            minChars: 1,
             source: function(term, suggest){
                 term = term.toLowerCase();
                 var choices = [['St. Mary\'s Hall'], ['Financial Aid'], ['Registrar'], ['Business Office'], ['Public Safety'], ['Cafeteria'], ['The Carl and Celia Berta Gellert Library'], ['Library Lawn'], ['Walter Gleason Gym'], ['Campus Center'], ['Academic Success Center'], ['Admissions'], ['International Student Office'], ['Student Life & Leadership Office'], ['The Quad'], ['Writing Center'], ['Cunningham Chapel Annex'], ['Center for Spirituality'], ['Julie Billiart Hall'], ['Dean of Students/Student Affairs'], ['Career Services'], ['St. Joseph Hall'], ['Housing'], ['The Apartments'], ['Koret Athletic Field'], ['Toso Residence (Compiegne)'], ['Human Resources'], ['Administration'], ['New Hall'], ['Counseling and Health Services'], ['Taube Center'], ['Theatre'], ['Ralston Hall'], ['Weigand Gallery'], ['Madison Art Center'], ['Cuvilly Hall'], ['Bookstore'], ['Gavin Hall'],['Province Center']];
